bda59a680d60c20a173daeebaba0a774d077604f
marclouisbruto <148203817+marclouisbruto@users.noreply.github.com>
2024-04-17 10:55:38 +0800

    package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a9b21b9eeec1ffdede5521ba8ee18c6c6c407151',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a9b21b9eeec1ffdede5521ba8ee18c6c6c407151',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'davmixcool/php-sentiment-analyzer' => array(
+            'pretty_version' => '1.2.2',
+            'version' => '1.2.2.0',
+            'reference' => 'd7503b68313c62eee3ed0ac9774c0a84d818a405',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../davmixcool/php-sentiment-analyzer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
