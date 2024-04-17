e7c86fef206c431e32ec14fdb7df49000493b2ed
patrickjohncedeno <cedenopatrickjohn@gmail.com>
2024-04-17 22:21:21 +0800

    a

--- index.php
+++ index.php
@@ -70,29 +70,14 @@ $sentimentAnalyzer = new SentimentAnalyzerWrapper();
                     <div class="col-md-4 style-3">
                         <div class="tour-item ">
                             <div class="tour-desc bg-white">
-                                <div class="tour-text color-grey-3 text-center"><?php
-                                    $text = "At this School, our mission is to balance a rigorous comprehensive college preparatory curriculum with healthy social and emotional development.";?> &ldquo;<?php $text; ?>&rdquo;</div>
+                                <div class="tour-text color-grey-3 text-center">
+                                        <label for="inp" class="inp">
+                                            <input type="text" id="inp" placeholder="&nbsp;">
+                                            <span class="label">Label</span>
+                                            <span class="focus-bg"></span>
+                                        </label>
+                                    <?php $text = "At this School, our mission is to balance a rigorous comprehensive college preparatory curriculum with healthy social and emotional development.";?> &ldquo;<?php $text; ?>&rdquo;</div>
                                 <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/6625914/pexels-photo-6625914.jpeg" alt=""></div>
-                                <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
-                                <div class="link-position d-flex justify-content-center">Student</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 style-3">
-                        <div class="tour-item ">
-                            <div class="tour-desc bg-white">
-                                <div class="tour-text color-grey-3 text-center">&ldquo;At this School, our mission is to balance a rigorous comprehensive college preparatory curriculum with healthy social and emotional development.&rdquo;</div>
-                                <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></div>
-                                <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
-                                <div class="link-position d-flex justify-content-center">Student</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 style-3">
-                        <div class="tour-item ">
-                            <div class="tour-desc bg-white">
-                                <div class="tour-text color-grey-3 text-center">&ldquo;At this School, our mission is to balance a rigorous comprehensive college preparatory curriculum with healthy social and emotional development.&rdquo;</div>
-                                <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/4946604/pexels-photo-4946604.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></div>
                                 <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
                                 <div class="link-position d-flex justify-content-center">Student</div>
                             </div>
